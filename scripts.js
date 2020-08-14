46c147cef70be9f5e90a79c27e30fbb9a508837d
Leobardo Arguelles <leobardomed@gmail.com>
2020-08-13 20:07:32 -0700

    Proyecto terminado

--- scripts.js
+++ scripts.js
@@ -9,6 +9,8 @@ startButton.addEventListener('click',addListener);
 window.addEventListener('resize',modifyGridPercentages);
 let lastGridDefinition;
 let flag = 0;
+let blackOpacity = 0;
+let blackOpacityFlag = true;
 
 let divsPerSide = 16;
 generateSquareGrid(16);
@@ -18,11 +20,25 @@ function changeFlag(){
 }
 
 function addListener(){
-    document.querySelectorAll('.grid').forEach(item => {
-        item.addEventListener('mouseenter', event =>{
-            item.classList.add('grid--black');
+    if(document.getElementById('black/white').checked){
+        document.querySelectorAll('.grid').forEach(item => {
+            item.addEventListener('mouseenter', event =>{
+                item.classList.add('grid--black');
+            })
+        })  
+    }else if(document.getElementById('rainbow').checked){
+        document.querySelectorAll('.grid').forEach(item => {
+            item.addEventListener('mouseenter', event =>{
+                changeDivBackground(item);
+            })
         })
-    })    
+    }else if(document.getElementById('grayscale').checked){
+        document.querySelectorAll('.grid').forEach(item => {
+            item.addEventListener('mouseenter', event =>{
+                changeDivColorGrayscale(item);
+            })
+        })
+    } 
 }
 
 function removeListener(){
@@ -35,10 +51,32 @@ function removeListener(){
             mydiv = document.createElement('div');
             mydiv.className = 'grid';
             mydiv.style.width = divs__width +'%';
-            mydiv.style.height = mydiv.clientWidth +'px';
+            mydiv.style.height = divs__width +'%';
             container.appendChild(mydiv);
         }
     }  
+}
+
+function generateSquareGrid(divs__width){
+    let mydiv;
+    clear();
+    createStartButton();
+    if(divs__width!==16){
+        divs__width = calculateDivsDimensions();
+    } else {
+        divs__width = calculateDivsDimensions(16);
+    }
+    lastGridDefinition = divsPerSide;
+    container = document.querySelector('.container');
+    for(let x = 0; x<divsPerSide; x++){   
+        for (let i=0; i<divsPerSide; i++){
+            mydiv = document.createElement('div');
+            mydiv.className = 'grid';
+            mydiv.style.width = divs__width +'%';
+            mydiv.style.height = divs__width +'%';
+            container.appendChild(mydiv);
+        }
+    }
 }
 
 function modifyGridPercentages() {
@@ -74,32 +112,10 @@ function calculateDivsDimensions(divsPerSide){
     container__width_noBorders = container__width - divsPerSide*2;
     divs__width = container__width_noBorders/divsPerSide;
     divs__width__percentage = divs__width*100/container__width;
-    console.log(divs__width__percentage);
     flag = 0;
     return divs__width__percentage;
 }
 
-function generateSquareGrid(divs__width){
-    let mydiv;
-    clear();
-    createStartButton();
-    if(divs__width!==16){
-        divs__width = calculateDivsDimensions();
-    } else {
-        divs__width = calculateDivsDimensions(16);
-    }
-    lastGridDefinition = divsPerSide;
-    container = document.querySelector('.container');
-    for(let x = 0; x<divsPerSide; x++){   
-        for (let i=0; i<divsPerSide; i++){
-            mydiv = document.createElement('div');
-            mydiv.className = 'grid';
-            mydiv.style.width = divs__width +'%';
-            mydiv.style.height = divs__width +'%';
-            container.appendChild(mydiv);
-        }
-    }
-}
 
 function clear(){
     container = document.querySelector('.container');
@@ -134,6 +150,39 @@ function askUserGridSize(){
                 'Please choose a smaller definition.')
     }
     return divsPerSide;
+}
+
+function randomRGB(){
+    return Math.random()*266;
+}
+
+function changeDivBackground(myDiv){
+    let r;
+    let g;
+    let b;
+    let rgb;
+    r = randomRGB();
+    g = randomRGB();
+    b = randomRGB();
+    rgb = 'rgb(' + r +',' + g +',' + b +')'
+    myDiv.style.backgroundColor = rgb;
+}
+
+function changeDivColorGrayscale(myDiv){
+    let rgb;
+    rgba = 'rgba(0,0,0,' + blackOpacity + ')';
+    myDiv.style.backgroundColor = rgba;
+    if (blackOpacityFlag){
+        blackOpacity += 0.1;
+        if (blackOpacity>=1){
+            blackOpacityFlag = false;
+        }
+    } else{
+        blackOpacity -= 0.1;
+        if (blackOpacity<=0){
+            blackOpacityFlag = true;
+        }
+    }
 }
 
 
